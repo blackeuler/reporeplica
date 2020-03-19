ae75c3c1e1eeb920eea8eb954f65a741583bccdd
Christopher Miller <miller1997@gmail.com>
2020-03-19 13:13:50 -0400

    settingUp oauth register

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask 
+from authlib.flask.client import Oauth
+from os import getenv
 
 app = Flask(__name__)
+
+oauth = Oauth(app)
+
+clientId = getenv("CLIENT_ID")
+clientSecret = getenv("ClIENT_SECRET")
+
+
+oauth.register(
+    name='github',
+    client_id=clientId,
+    client_secret=clientSecret,
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize',
+    api_base_url='https://api.github.com/',
+    client_kwargs={'scope': 'repo'},
+)
+
 
 @app.route('/')
 def index():
